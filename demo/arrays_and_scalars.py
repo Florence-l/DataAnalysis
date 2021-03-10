8b38270678ec9e9717f272bef220b50704de3c95
Florence-l <1070883368@qq.com>
2021-03-10 16:31:04 +0800

    'first_commit'

---
+++ demo/arrays_and_scalars.py
@@ -0,0 +1,23 @@
+import numpy as np
+arr1 = np.array([1, 2, 3, 4], [6, 7, 8, 9])
+arr1
+arr1 * arr1
+1 / arr1
+arr1 - arr1
+arr1 ** 3
+
+
+arr = np.arange(0, 11)
+arr
+arr[8]
+arr[1:5]
+arr[0:5] = 100
+arr
+slice_of_arr = arr[0:7]
+slice_of_arr
+
+arr_copy = arr.copy()
+arr_copy
+
+arr_2d = np.array([5, 0, 15], [20, 25])
+5
